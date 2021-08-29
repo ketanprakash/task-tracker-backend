35be1298def495660f4097892a8d0b8aa6b60d12
ketanprakash <ketanprakash001@gmail.com>
2021-08-30 01:37:51 +0530

    created website

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-const { port } = require('./config/config')
-// const port = process.env.PORT;
+const { port, user, password, pgport } = require('./config/config')
 const express = require('express');
-// const cors = require('cors');
-// const { tasks } = require('./routes/tasks');
+const cors = require('cors');
+const { tasks } = require('./routes/tasks');
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-
-// app.use('/tasks', tasks);
+app.use(cors());
+app.use(express.json());
+console.log()
+app.use('/tasks', tasks);
 
 app.get('*', (req, res) => {
   res.status(200).send(`Started Server`);
