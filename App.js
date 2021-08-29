16b995c9587b68272e80d8f174b38e9d9f75c295
ketanprakash <ketanprakash001@gmail.com>
2021-08-30 01:43:06 +0530

    created website

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-const { port, user, password, pgport } = require('./config/config')
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const { tasks } = require('./routes/tasks');
 const app = express();
 
+const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
-console.log(port, user, password, pgport);
+
 app.use('/tasks', tasks);
 
 app.get('*', (req, res) => {
   res.status(200).send(`Started Server`);
 });
 
-app.listen(process.env.PORT, (err) => {
+app.listen(port, (err) => {
   if (err) console.log("Error in server setup")
   console.log("Server listening on Port");
 })
--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
-const { user, host, database, password, pgport } = require('./config')
 const { Client } = require('pg');
 
-const client = new Client({ user, host, database, password, port: pgport });
+const client = new Client({ 
+  user: process.env.PGUSER, 
+  host: process.env.PGHOST, 
+  database: process.env.PGDATABASE, 
+  password: process.env.PGPASSWORD, 
+  port: process.env.PGPORT
+});
 client.connect();
 
 module.exports.client = client;
