44bba240fe8b4952836b82612afae096b168f036
ketanprakash <ketanprakash001@gmail.com>
2021-08-30 01:07:09 +0530

    created website

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
-const { port } = require('./config/config')
+// const { port } = require('./config/config')
 const express = require('express');
-const cors = require('cors');
-const { tasks } = require('./routes/tasks');
+// const cors = require('cors');
+// const { tasks } = require('./routes/tasks');
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-app.use('/tasks', tasks);
+// app.use('/tasks', tasks);
 
-app.get('*', (req, res) => {
-  res.status(200).send(`Started Server on port ${port}`);
-});
+// app.get('*', (req, res) => {
+//   res.status(200).send(`Started Server on port ${port}`);
+// });
 
-app.listen(port, (err) => {
+app.listen(process.env.PORT, (err) => {
   if (err) console.log("Error in server setup")
   console.log("Server listening on Port", PORT);
 })
