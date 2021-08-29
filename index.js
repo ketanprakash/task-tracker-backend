718308ead3c7e3613d68af374297e96065e73355
ketanprakash <ketanprakash001@gmail.com>
2021-08-30 01:51:15 +0530

    created website

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 const express = require('express');
-// const cors = require('cors');
-const { tasks } = require('./routes/tasks');
-const app = express();
+// // const cors = require('cors');
+// const { tasks } = require('./routes/tasks');
+// const app = express();
 
-const port = process.env.PORT || 8000;
-// app.use(cors());
-app.use(express.json());
+// const port = process.env.PORT || 8000;
+// // app.use(cors());
+// app.use(express.json());
 
-app.use('/tasks', tasks);
+// app.use('/tasks', tasks);
 
 app.get('*', (req, res) => {
   res.status(200).send(`Started Server`);
 });
-
+const port = process.env.PORT || 8000;
 app.listen(port, (err) => {
   if (err) console.log("Error in server setup")
   console.log("Server listening on Port");
